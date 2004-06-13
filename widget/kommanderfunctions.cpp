bb8818cb95c0b3cca3f97f4ddb89b5dee1278ede
Michal Rudolf <mrudolf@kdewebdev.org>
2004-06-13 08:40:48 +0000

    Fix nasty bug in execBegin.
    svn path=/trunk/kdewebdev/kommander/; revision=320193

--- widget/kommanderfunctions.cpp
+++ widget/kommanderfunctions.cpp
@@ -78,9 +78,9 @@ QString KommanderWidget::evalExecBlock(const QStringList& args, const QString& s
   else
   {
     QString shell = args.count() ? args[0] : QString::null;
-    int size = f - pos;
-    pos = f + QString("@execEnd").length();
-    return execCommand(evalAssociatedText(s.mid(pos, size)), shell);
+    int start = pos;
+    pos = f + QString("@execBegin").length();
+    return execCommand(evalAssociatedText(s.mid(start, f - start)), shell);
   }
 }
  
