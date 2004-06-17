15061068930b0045b191dee5e6acacfeb6fe5a48
Michal Rudolf <mrudolf@kdewebdev.org>
2004-06-17 16:12:15 +0000

    Add preliminary specials descriptions needed for future function browser.
    svn path=/trunk/kdewebdev/kommander/; revision=321451

--- executor/register.cpp
+++ executor/register.cpp
@@ -42,51 +42,56 @@ void Instance::registerDCOP()
 void Instance::registerSpecials()
 {
   /* global specials */
-  SpecialInformation::insert("widgetText");
-  SpecialInformation::insert("selectedWidgetText");
-  SpecialInformation::insert("pid");
-  SpecialInformation::insert("dcopid");
-  SpecialInformation::insert("parentPid");
+  SpecialInformation::insert("widgetText", 0, 0);
+  SpecialInformation::insert("selectedWidgetText", 0, 0);
+  SpecialInformation::insert("pid", 0, 0);
+  SpecialInformation::insert("dcopid", 0, 0);
+  SpecialInformation::insert("parentPid", 0, 0);
   SpecialInformation::insert("execBegin", 0, 1);
-  SpecialInformation::insert("env", 1);
-  SpecialInformation::insert("exec", 1);
-  SpecialInformation::insert("global", 1);
+  SpecialInformation::insert("env", 1, 1);
+  SpecialInformation::insert("exec", 1, 1);
+  SpecialInformation::insert("global", 1, 1);
   SpecialInformation::insert("dialog", 1, 2);
-  SpecialInformation::insert("readSetting", 2);  
-  SpecialInformation::insert("setGlobal", 2);
-  SpecialInformation::insert("writeSetting", 2);
+  SpecialInformation::insert("readSetting", 2, 2);  
+  SpecialInformation::insert("setGlobal", 2, 2);
+  SpecialInformation::insert("writeSetting", 2, 2);
   SpecialInformation::insert("dcop", 4, 10);
   
   /* array specials */
   SpecialInformation::setCurrentObject("Array");
-  SpecialInformation::insert("values", 1);
-  SpecialInformation::insert("keys", 1);
-  SpecialInformation::insert("clear", 1);
-  SpecialInformation::insert("count", 1);
-  SpecialInformation::insert("value", 2);
-  SpecialInformation::insert("remove", 2);
-  SpecialInformation::insert("setValue", 3);
-  SpecialInformation::insert("read", 2);
-  SpecialInformation::insert("write", 2);
+  SpecialInformation::insert("values", 1, 1, "Return EOL-separated list of all values in the array.");
+  SpecialInformation::insert("keys", 1, 1, "Return EOL-separated list of all keys in the array.");
+  SpecialInformation::insert("clear", 1, 1, "Remove all elements from the array.");
+  SpecialInformation::insert("count", 1, 1, "Return number of elements in the array.");
+  SpecialInformation::insert("value", 2, 2, "Return the value associated with given key.");
+  SpecialInformation::insert("remove", 2, 2, "Remove element with given key from the array.");
+  SpecialInformation::insert("setValue", 3, 3, "Add element with given key and value to the array");
+  SpecialInformation::insert("read", 2, 2, "Add all elements in the string to the array. "
+    "String should have <i>key>\\tvalue\\n</i> format.");
+  SpecialInformation::insert("write", 2, 2, "Return all elements in the array in "
+    "<i>key>\\tvalue\\n</i> format.");
   
   SpecialInformation::setCurrentObject("String");
-  SpecialInformation::insert("length", 1);
-  SpecialInformation::insert("contains", 2);
-  SpecialInformation::insert("find", 2);
-  SpecialInformation::insert("left", 2);
-  SpecialInformation::insert("right", 2);
-  SpecialInformation::insert("mid", 3);
-  SpecialInformation::insert("remove", 2);
-  SpecialInformation::insert("replace", 3);
-  SpecialInformation::insert("lower", 1);
-  SpecialInformation::insert("upper", 1);
-  SpecialInformation::insert("lower", 1);
-  SpecialInformation::insert("compare", 2);
-  SpecialInformation::insert("isEmpty", 1);
-  SpecialInformation::insert("isNumber", 1);
+  SpecialInformation::insert("length", 1, 1, "Return number of chars in the string.");
+  SpecialInformation::insert("contains", 2, 2, "Check if the string contains given substring.");
+  SpecialInformation::insert("find", 2, 2, "Return position of a substring in the string, "
+    "or -1 if it isn't found.");
+  SpecialInformation::insert("left", 2, 2, "Return first n chars of the string.");
+  SpecialInformation::insert("right", 2, 2, "Return last n chars of the string.");
+  SpecialInformation::insert("mid", 3, 3, "Return substring of the string, starting from "
+    "given position.");
+  SpecialInformation::insert("remove", 2, 2, "Replace all occurencies of given substring.");
+  SpecialInformation::insert("replace", 3, 3, "Replace all occurencies of given substring "
+    "with given replacement.");
+  SpecialInformation::insert("upper", 1, 1, "Convert the string to uppercase.");
+  SpecialInformation::insert("lower", 1, 1, "Convert the string to lowercase.");
+  SpecialInformation::insert("compare", 2, 2, "Compare two strings. Return 0 if they are equal, "
+    "-1 if the first one is lower, 1 if the first one is higher");
+  SpecialInformation::insert("isEmpty", 1, 1, "Check if string is empty.");
+  SpecialInformation::insert("isNumber", 1, 1, "Check if string is a valid number.");
   
   SpecialInformation::setCurrentObject("File");
-  SpecialInformation::insert("read", 1);
-  SpecialInformation::insert("write", 2);
-  SpecialInformation::insert("append", 2);
+  SpecialInformation::insert("read", 1, 1, "Return content of given file.");
+  SpecialInformation::insert("write", 2, 2, "Write given string to a file.");
+  SpecialInformation::insert("append", 2, 2, "Append given string to the end of a file.");
 }
