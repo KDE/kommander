d52e83106c3d3ab861834b95bb6ffad7e49e9e26
Michal Rudolf <mrudolf@kdewebdev.org>
2004-07-08 19:17:23 +0000

    Add StatusBar widget.
    svn path=/trunk/kdewebdev/kommander/; revision=327005

---
+++ widgets/statusbar.cpp
@@ -0,0 +1,109 @@
+/***************************************************************************
+                         statusbar.cpp - StatusBar widget 
+                             -------------------
+    copyright            : (C) 2004      Michal Rudolf <mrudolf@kdewebdev.org>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+/* KDE INCLUDES */
+
+/* QT INCLUDES */
+#include <qobject.h>
+#include <qstring.h>
+#include <qwidget.h>
+#include <qstringlist.h>
+#include <qevent.h>
+
+/* OTHER INCLUDES */
+#include <specials.h>
+#include "statusbar.h"
+
+StatusBar::StatusBar(QWidget *a_parent, const char *a_name)
+  : KStatusBar(a_parent, a_name), KommanderWidget(this)
+{
+  QStringList states;
+  states << "default";
+  setStates(states);
+  setDisplayStates(states);
+  insertItem(QString::null, 0, 1);
+  setItemAlignment(0, Qt::AlignLeft);
+}
+
+StatusBar::~StatusBar()
+{
+}
+
+QString StatusBar::currentState() const
+{
+  return QString("default");
+}
+
+bool StatusBar::isKommanderWidget() const
+{
+  return TRUE;
+}
+
+QStringList StatusBar::associatedText() const
+{
+  return KommanderWidget::associatedText();
+}
+
+void StatusBar::setAssociatedText(const QStringList& a_at)
+{
+  KommanderWidget::setAssociatedText(a_at);
+}
+
+void StatusBar::setPopulationText(const QString& a_text)
+{
+  KommanderWidget::setPopulationText(a_text);
+}
+
+QString StatusBar::populationText() const
+{
+  return KommanderWidget::populationText();
+}
+
+void StatusBar::populate()
+{
+  message(KommanderWidget::evalAssociatedText( populationText()));
+}
+
+void StatusBar::showEvent(QShowEvent *e)
+{
+  QStatusBar::showEvent(e);
+  emit widgetOpened();
+}
+
+QString StatusBar::handleDCOP(int function, const QStringList& args)
+{
+  switch (function) {
+    case DCOP::setText:
+      changeItem(args[0], 0);
+      break;
+    case DCOP::insertItem:
+      if (hasItem(args[1].toInt()))
+        changeItem(args[0], args[1].toInt());
+      else 
+        insertItem(args[0], args[1].toInt());
+      break;
+    case DCOP::removeItem:
+      removeItem(args[0].toInt());
+      break;
+    case DCOP::clear:
+      clear();
+      break;
+    default:
+      break;
+  }
+  return QString::null;
+}
+
+#include "statusbar.moc"
---
+++ widgets/statusbar.h
@@ -0,0 +1,62 @@
+/***************************************************************************
+                          statusbar.h - StatusBar widget 
+                             -------------------
+    copyright            : (C) 2004      Michal Rudolf <mrudolf@kdewebdev.org>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _HAVE_STATUSBAR_H_
+#define _HAVE_STATUSBAR_H_
+
+/* KDE INCLUDES */
+
+/* QT INCLUDES */
+#include <qstringlist.h>
+#include <qobject.h>
+#include <qstring.h>
+
+/* OTHER INCLUDES */
+#include <kstatusbar.h>
+#include <kommanderwidget.h>
+
+class QWidget;
+
+class QShowEvent;
+class StatusBar : public KStatusBar, public KommanderWidget
+{
+  Q_OBJECT
+
+  Q_PROPERTY(QString populationText READ populationText WRITE setPopulationText DESIGNABLE false)
+  Q_PROPERTY(QStringList associations READ associatedText WRITE setAssociatedText DESIGNABLE false)
+  Q_PROPERTY(bool KommanderWidget READ isKommanderWidget)
+  
+public:
+  StatusBar(QWidget *a_parent, const char *a_name);
+  ~StatusBar();
+
+   virtual bool isKommanderWidget() const;
+  virtual void setAssociatedText(const QStringList&);
+  virtual QStringList associatedText() const;
+  virtual QString currentState() const;
+  virtual QString populationText() const;
+  virtual void setPopulationText(const QString&);
+
+  virtual QString handleDCOP(int function, const QStringList& args);
+public slots:
+  virtual void populate();
+signals:
+  void widgetOpened();
+protected:
+  void showEvent(QShowEvent *e);
+private:
+};
+
+#endif
