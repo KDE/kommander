252b03fe3466d7de32cac1880a0220b5ef2bc56e
Michal Rudolf <mrudolf@kdewebdev.org>
2004-12-20 22:02:15 +0000

    Add basic Table widget.
    svn path=/trunk/kdewebdev/kommander/; revision=372378

---
+++ widgets/table.cpp
@@ -0,0 +1,109 @@
+/***************************************************************************
+                          table.cpp - Table widget
+                             -------------------
+    copyright            : (C) 2004      Michal Rudolf <mrudolf@kdewebdev.org>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+/* KDE INCLUDES */
+
+/* QT INCLUDES */
+#include <qstring.h>
+#include <qwidget.h>
+#include <qstringlist.h>
+
+/* OTHER INCLUDES */
+#include <specials.h>
+#include "table.h"
+
+Table::Table(QWidget *a_parent, const char *a_name)
+  : QTable(a_parent, a_name), KommanderWidget(this)
+{
+  QStringList states;
+  states << "default";
+  setStates(states);
+  setDisplayStates(states);
+}
+
+Table::~Table()
+{
+}
+
+QString Table::currentState() const
+{
+  return QString("default");
+}
+
+bool Table::isKommanderWidget() const
+{
+  return TRUE;
+}
+
+QStringList Table::associatedText() const
+{
+  return KommanderWidget::associatedText();
+}
+
+void Table::setAssociatedText(const QStringList& a_at)
+{
+  KommanderWidget::setAssociatedText(a_at);
+}
+
+void Table::setPopulationText(const QString& a_text)
+{
+  KommanderWidget::setPopulationText(a_text);
+}
+
+QString Table::populationText() const
+{
+  return KommanderWidget::populationText();
+}
+
+void Table::populate()
+{
+  setWidgetText(KommanderWidget::evalAssociatedText( populationText()));
+}
+
+void Table::setWidgetText(const QString& a_text)
+{
+}
+
+bool Table::isFunctionSupported(int f)
+{
+  return f == DCOP::currentColumn || f == DCOP::currentRow || f == DCOP::insertColumn 
+      || f == DCOP::insertRow || f == DCOP::cellText || f == DCOP::setCellText;
+}
+
+QString Table::handleDCOP(int function, const QStringList& args)
+{
+  switch (function) {
+    case DCOP::cellText:
+      return text(args[0].toInt(), args[1].toInt());
+    case DCOP::setCellText:
+      setText(args[0].toInt(), args[1].toInt(), args[2]);
+      break;
+    case DCOP::insertRow:
+      insertRows(args[0].toInt(), args.count());
+      break;
+    case DCOP::insertColumn:
+      insertColumns(args[0].toInt(), args.count());
+      break;
+    case DCOP::currentColumn:
+      return QString::number(currentColumn());
+    case DCOP::currentRow:
+      return QString::number(currentRow());
+  }  
+  return QString::null;
+}
+
+#include "table.moc"
+
---
+++ widgets/table.h
@@ -0,0 +1,60 @@
+/***************************************************************************
+                          table.h - Table widget
+                             -------------------
+    copyright            : (C) 2004      Michal Rudolf <mrudolf@kdewebdev.org>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#ifndef _HAVE_TABLE_H_
+#define _HAVE_TABLE_H_
+
+/* KDE INCLUDES */
+
+/* QT INCLUDES */
+#include <qtable.h>
+#include <qobject.h>
+
+/* OTHER INCLUDES */
+#include <kommanderwidget.h>
+
+class QWidget;
+
+class Table : public QTable, public KommanderWidget
+{
+  Q_OBJECT
+
+      Q_PROPERTY(QString populationText READ populationText WRITE setPopulationText DESIGNABLE false)
+      Q_PROPERTY(QStringList associations READ associatedText WRITE setAssociatedText DESIGNABLE false)
+      Q_OVERRIDE(bool readOnly DESIGNABLE false)
+      Q_PROPERTY(bool KommanderWidget READ isKommanderWidget)
+
+  public:
+    Table(QWidget *a_parent, const char *a_name);
+    ~Table();
+
+    virtual bool isKommanderWidget() const;
+    virtual void setAssociatedText(const QStringList&);
+    virtual QStringList associatedText() const;
+    virtual QString currentState() const;
+
+    virtual QString populationText() const;
+    virtual void setPopulationText(const QString&);
+
+    virtual QString handleDCOP(int function, const QStringList& args);
+    virtual bool isFunctionSupported(int function);
+  public slots:
+    virtual void populate();
+    virtual void setWidgetText(const QString &);
+};
+
+#endif
+
