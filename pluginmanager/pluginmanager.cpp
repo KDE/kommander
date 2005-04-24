98025290c09977a6816367264fe9406d3a4f1315
Michal Rudolf <mrudolf@kdewebdev.org>
2005-04-24 19:00:50 +0000

    Separate plugin handling from GUI so that command line plugin management is possible - add missing files.
    svn path=/trunk/kdewebdev/kommander/; revision=407591

---
+++ pluginmanager/pluginmanager.cpp
@@ -0,0 +1,89 @@
+/***************************************************************************
+                          pluginmanager.cpp  -  Plugin manager class
+                             -------------------
+    copyright            : (C) 2005 Michal Rudolf <mrudolf@kdewebdev.org>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include <kconfig.h>
+#include <klibloader.h>
+ 
+#include "pluginmanager.h"
+
+PluginManager::PluginManager()
+{
+  m_cfg = new KConfig("kommanderrc");
+  m_list = new QStringList(m_cfg->readListEntry("plugins"));
+}
+
+PluginManager::~PluginManager()
+{
+  m_cfg->writeEntry("plugins", *m_list);
+  delete m_cfg;
+  delete m_list;
+}  
+  
+int PluginManager::count() const
+{
+  return m_list->count();  
+}
+  
+QString PluginManager::item(int i) const
+{
+  return (*m_list)[i];
+}
+    
+bool PluginManager::add(const QString& plugin, bool)
+{
+  QString plugName= libraryName(plugin);
+  if (plugName.isNull())
+    return false;
+  if (m_list->contains(plugName))
+    return false;
+  m_list->append(plugName);
+  return true;
+}
+
+bool PluginManager::remove(const QString& plugin)
+{
+  if (!m_list->contains(plugin))
+    return false;  
+  m_list->remove(plugin);
+  return true;
+}
+
+int PluginManager::verify()
+{
+  QStringList verified;
+  for (QStringList::ConstIterator it = m_list->begin(); it != m_list->end(); ++it)
+    if (!libraryName(*it).isNull())
+      verified.append(*it);
+  int removed = count() - verified.count();
+  m_list->clear();
+  (*m_list) += verified;
+  return removed;
+}
+
+QString PluginManager::libraryName(const QString& plugin) const
+{
+  KLibrary *l = KLibLoader::self()->library(plugin.latin1());
+  QString libname;
+  if (l && l->hasSymbol("kommander_plugin"))
+    libname = l->fileName();
+  return libname;
+} 
+
+QStringList PluginManager::items() const
+{
+  return (*m_list);
+}
+
---
+++ pluginmanager/pluginmanager.h
@@ -0,0 +1,53 @@
+/***************************************************************************
+                          pluginmanager.h  -  Plugin manager class
+                             -------------------
+    copyright            : (C) 2005 Michal Rudolf <mrudolf@kdewebdev.org>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+ 
+#ifndef __PLUGINMANAGER__
+#define __PLUGINMANAGER__
+
+#include <qstringlist.h>
+
+class KConfig;
+
+class PluginManager
+{
+public:  
+  PluginManager();
+  ~PluginManager();
+  
+  // number of known plugins
+  int count() const;
+  // n-th plugin
+  QString item(int i) const;
+  // adds a plugin if valid; 
+  // FIXME: if overwrite is true, plugin with the same base name will be removed
+  bool add(const QString& plugin, bool overwrite = true);
+  // removes given plugin
+  bool remove(const QString& plugin);
+  // remove invalid plugins, returns number of removed plugins
+  int verify();
+  // list of plugins
+  QStringList items() const;
+private:
+  // returns expanded library name or null if it is invalid
+  QString libraryName(const QString& plugin) const;
+  
+  QStringList* m_list;
+  KConfig *m_cfg;
+};
+
+
+#endif
+
